9c8e8aa8bf2d2876bbf8ff82395ec7c92c1ed121
Maslia Qomar <masliaqomar@Maslias-MacBook-Air.local>
2022-09-20 17:03:55 +0800

    kalender_hij.py

---
+++ phyton 30/Tugas/kalender_hij.py
@@ -0,0 +1,60 @@
+print("---Menentukan Bulan Hijriah berdasarkan kalender Masehi 2022---")
+tanggal = int(input("Masukkan Tanggal : "))
+bulan = int(input("Masukkan Bulan : "))
+tahun = int(input("Masukkan Tahun : "))
+
+if tanggal >= 1 and tanggal <= 4 and bulan == 1 and tahun == 2022:
+    print("Jumada Ula 1443 H")
+elif tanggal >= 5 and tanggal <= 31 and bulan == 1 and tahun == 2022:
+    print("Jumada Tsani 1443 H")
+elif tanggal >= 1 and tanggal <= 2 and bulan ==2 and tahun == 2022:
+    print("Jumada tsani 1443 H")
+elif tanggal >= 3 and tanggal <= 28 and bulan == 2 and tahun == 2022:
+    print("Rajab 1443 H")
+elif tanggal >= 1 and tanggal <= 4 and bulan == 3 and tahun == 2022:
+    print("Rajab 1443 H")
+elif tanggal >= 5 and tanggal <= 31 and bulan == 3 and tahun == 2022:
+    print("Sya'ban 1443 H")
+elif tanggal >= 1 and tanggal <= 2 and bulan == 4 and tahun == 2022:
+    print("Sya'ban 1443 H")
+elif tanggal >= 3 and tanggal <= 30 and bulan == 4 and tahun == 2022:
+    print("Ramadhan 1443 H")
+elif tanggal >= 1 and tanggal <= 2 and bulan == 5 and tahun == 2022:
+    print("Ramadhan 1443 H")
+elif tanggal >= 3 and tanggal <= 31 and bulan == 5 and tahun == 2022:
+    print("Syawwal 1443 H")
+elif tanggal >= 1 and tanggal <= 30 and bulan == 6 and tahun ==2022:
+    print("Dzulqo'dah 1443 H")
+elif tanggal >= 1 and tanggal <= 30 and bulan == 7 and tahun ==2022:
+    print("Dzulhijjah 1443 H")
+elif tanggal >= 31 and tanggal <= 31 and bulan == 7 and tahun == 2022:
+    print("Muharram 1444 H")
+elif tanggal >= 1 and tanggal <= 28 and bulan == 8 and tahun == 2022:
+    print("Muharram 1444 H")
+elif tanggal >= 29 and tanggal <= 31 and bulan == 8 and tahun == 2022:
+    print("Safar 1444 H")
+elif tanggal >= 1 and tanggal <= 27 and bulan == 9 and tahun == 2022:
+    print("Safar 1444 H")
+elif tanggal >= 28 and tanggal <= 30 and bulan == 9 and tahun == 2022:
+    print("Rabi'al awwal 1444 H")
+elif tanggal >= 1 and tanggal <= 26 and bulan == 10 and tahun == 2022:
+    print("Rabi'al awwal 1444 H")
+elif tanggal >= 27 and tanggal <= 31 and bulan == 10 and tahun == 2022:
+    print("Rabia Tsani 1444 H")
+elif tanggal >= 1 and tanggal <= 25 and bulan == 11 and tahun == 2022:
+    print("Rabia Tsani 1444 H")
+elif tanggal >= 26 and tanggal <= 30 and bulan == 11 and tahun == 2022:
+    print("Jumada Ula 1444 H")
+elif tanggal >= 1 and tanggal <= 25 and bulan == 12 and tahun == 2022:
+    print("Jumada Ula 1444 H")
+elif tanggal >= 26 and tanggal <= 31 and bulan == 12 and tahun == 2022:
+    print("Jumada Tsani 1444 H")
+elif tanggal >= 1 and tanggal <= 24 and bulan == 1 and tahun == 2023:
+    print("Jumada Tsani 1444 H")
+else:
+    print("Tanggal yang anda masukkan salah")
+    print("Bulan yang anda masukkan salah")
+    
+
+
+
