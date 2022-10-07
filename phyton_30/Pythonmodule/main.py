3c2699ead1a94a753bc6c2501e6d265dc0976f76
Lhiya <masliaqomar@Maslias-MacBook-Air.local>
2022-10-07 18:34:35 +0800

    phyton_30

---
+++ phyton_30/Pythonmodule/main.py
@@ -0,0 +1,19 @@
+#cara 1
+#from Math.formulas import Hitung, hitung_luas_lingkaran
+
+#cara 2
+from Math.formulas import * #all modules
+
+
+#j1 = Hitung() #1 dan 2
+#j1 = j1.jumlah(3,4)
+#print(j1)
+
+#luas_lingkaran1 = hitung_luas_lingkaran(10)
+#print(f"Luas Lingkaran 1 adalah : {luas_lingkaran1}")
+
+#cara 3
+import Math
+
+m = Math.formulas.Hitung().jumlah(4,5)
+print(m)
